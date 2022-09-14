1a715db1d039d8affa733248d6078ea05c992249
carlatakagi <carlatakagi@gmail.com>
2022-09-13 22:59:39 -0400

    feat: linear gradient

--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -4,8 +4,14 @@ import logo from './assets/logo-nlw.svg';
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="logo da nlw eSports" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu
+        <span className="bg-nlw-gradient text-transparent bg-clip-text"> duo </span>
+        está aqui.
+      </h1>
     </div>
   )
 }
