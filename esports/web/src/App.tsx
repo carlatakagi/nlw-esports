e17955cbd0eabea459a9d9c8771019d5b1cc2b00
carlatakagi <carlatakagi@gmail.com>
2022-09-14 21:24:20 -0400

    feat: alterações no app

--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -1,4 +1,5 @@
 import './styles/main.css';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 import logo from './assets/logo-nlw.svg';
 
@@ -57,6 +58,20 @@ function App() {
             </div>
         </a>
       </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden"></div>
+        <div className="bg-[#2A2634] px-8 py-6 flex self-stretch justify-between items-center">
+            <div>
+                <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+                <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+            </div>
+            <div>
+                <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+                    <MagnifyingGlassPlus size="24"></MagnifyingGlassPlus>
+                    Publicar anúncio
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
